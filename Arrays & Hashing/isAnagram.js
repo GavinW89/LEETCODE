68c4f0825e41b8852507cdd43e4592de66bf3167
Gavin Woods <gav5400@gmail.com>
2024-06-27 12:00:39 -0400

    first interation of anagrams

--- Arrays & Hashing/isAnagram.js
+++ Arrays & Hashing/isAnagram.js
@@ -3,16 +3,16 @@ function isAnagram(s, t){
     for (const x in s){ //loop through first string
         var match = false;
         for(const z in t){ //loop through second string
-            if(s[x] == t[z]){
+            if(s[x] == t[z]){ // first letter of first string compared to every letter of second string
                 match = true;
                 exit
             }else{
-                return false;
+                return false; // no match then return false
             }
         }
         if(match == false) return false;
     }
-    return true
+    return true //if makes through both loops then its an anagram return true
 }
 
 console.log(isAnagram('racecar', 'carrace'))
